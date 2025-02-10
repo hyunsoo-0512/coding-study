53531a4a497b275181200a8df1ce0721502a3610
hyunsoo-0512 <tttirwy5558426@gmail.com>
2025-02-10 19:55:56 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -0,0 +1,15 @@
+task = []
+no_task = []
+for i in range(28):
+    a = int(input())
+    task.append(a)
+    
+for o in range(1,31):
+    a=task.count(o)
+    if a==0:
+        no_task.append(o)
+
+no_task.sort()
+
+print(no_task[0])
+print(no_task[1])
