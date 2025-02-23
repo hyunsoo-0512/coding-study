432cc29a6b1c80675f06e0f57b85d84668f9b38d
hyunsoo-0512 <tttirwy5558426@gmail.com>
2025-02-24 06:00:04 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,12 +1,12 @@
-a = input().strip()
-b = ["dz=", "c=", "c-", "d-", "lj", "nj", "s=", "z="]
-count = 0
+a=input().strip()
+b=['dz=','c=','c-','d-','lj','nj','s=','z=']
+e=0
 
-for cro in b:
-    while cro in a:
-        count += 1
-        a = a.replace(cro, " ", 1) 
 
-count += len(a.replace(" ", "")) 
-
-print(count)
+for i in b:
+  while i in a:
+    e += 1
+    a= a.replace(i, ' ', 1)
+  
+e += len(a.replace(' ', ''))
+print(e)
