03eda0c6af6c52d44ce6ddf23de3a8b6c480eb37
hyunsoo-0512 <tttirwy5558426@gmail.com>
2025-03-26 14:26:58 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,15 +1,13 @@
 t=int(input())
 for i in range(t):
     c=int(input())
-    for i in range(4):
-        q=c//25
-        q_r=c%25
+    q=c//25
+    q_r=c%25
 
-        d=q_r//10
-        d_r=q_r%10
+    d=q_r//10
+    d_r=q_r%10
 
-        n=d_r//5
+    n=d_r//5
 
-        p=d_r%5
-
+    p=d_r%5
     print(q,d,n,p)
